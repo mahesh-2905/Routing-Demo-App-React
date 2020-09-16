b45285af79b4daeb792b1e8bfefd435f8b8b0f0c
Maheshkumar <65496812+mahesh-2905@users.noreply.github.com>
2020-09-16 18:37:29 +0530

    Profile page

---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+
+
+class Profile extends Component{
+
+    constructor(){
+        super()
+        this.state={
+
+        }
+       
+    }
+    render(){
+        return(
+            <div class="jumbotron text-center container">
+                <h1 class="display-3 ">Profile Component</h1>
+                <hr class="my-2"/>
+                <p class="lead">Here is th another new component...!</p>
+            </div>
+        )
+    }
+}
+
+export default Profile;
